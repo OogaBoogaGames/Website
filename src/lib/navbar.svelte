3c013c569bbb624b9845d31ad9780f60b1169ebd
endercass <46408156+Endercass@users.noreply.github.com>
2023-06-26 01:59:09 -0400

    Rewrite stylesheets and add a layout file

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,11 @@
+<div class="nav-bar">
+	<!--TODO-->
+</div>
+
+<style lang="scss">
+	.nav-bar {
+		width: 100%;
+		height: 8vh;
+		background: rgba(#49a4d8, 0.85);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	let character_sprites = ["/sprite1.webp", "/sprite2.webp"]
+	let character_sprites = ['/sprite1.webp', '/sprite2.webp'];
 	let character_index = 0;
 	let inputValue = '';
 
@@ -13,52 +13,52 @@
 	function leftButtonClick() {
 		character_index--;
 		if (character_index < 0) {
-			character_index = character_sprites.length-1;
+			character_index = character_sprites.length - 1;
 		}
 	}
 
 	function rightButtonClick() {
 		character_index++;
-		if (character_index > character_sprites.length-1) {
+		if (character_index > character_sprites.length - 1) {
 			character_index = 0;
 		}
 	}
 </script>
 
-<div class="login-container">
-    <h1>Ooga Booga Games</h1>
+<div class="front-page">
+	<h1>Ooga&nbsp;Booga&nbsp;Games</h1>
 
-    <div class="form-container">
-        <div class="character-form">
-            <img src={character_sprites[character_index]} alt="character" />
-            <div class="character-form-buttons">
-            	<button on:click={leftButtonClick}>←</button>
-            	<button on:click={rightButtonClick}>→</button>
-            </div>
-        </div>
+	<div class="form-container">
+		<div class="character-form">
+			<img src={character_sprites[character_index]} alt="character" />
+			<div class="character-form-buttons">
+				<button on:click={leftButtonClick}>←</button>
+				<button on:click={rightButtonClick}>→</button>
+			</div>
+		</div>
 
-        <div class="room-form">
-        	<input
-        		type="text"
-        		autocapitalize="none"
-        		autocorrect="off"
-        		autocomplete="off"
-        		placeholder="ENTER ROOM CODE"
-        		maxlength={8}
-        		bind:value={inputValue}
-        		on:input={handleInput}
-        	/>
-        	<input
-        		type="text"
-        		autocapitalize="none"
-        		autocorrect="off"
-        		autocomplete="off"
-        		placeholder="ENTER YOUR NAME"
-        		maxlength={16}
-        	/>
-        	<button>Join Game</button>
-        </div>
-    </div>
+		<div class="room-form">
+			<input
+				type="text"
+				autocapitalize="none"
+				autocorrect="off"
+				autocomplete="off"
+				placeholder="ENTER ROOM CODE"
+				maxlength={8}
+				bind:value={inputValue}
+				on:input={handleInput}
+			/>
+			<input
+				type="text"
+				autocapitalize="none"
+				autocorrect="off"
+				autocomplete="off"
+				placeholder="ENTER YOUR NAME"
+				maxlength={16}
+			/>
+			<button>Join Game</button>
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
@@ -66,107 +66,95 @@
 	$primary-color: #6a787e;
 	$secondary-color: #9bad8c;
 	$background-color: #363239;
-	:root {
-		background: url('https://media.discordapp.net/attachments/1121939717458514020/1122454022893338736/s.png');
-		background-position: center;
-		background-repeat: no-repeat;
-		background-size: cover;
-		backdrop-filter: blur(4px);
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		height: 100%;
-		font-family: Poppins, sans-serif;
+
+	.front-page {
+		padding: 5%;
+
+		h1 {
+			color: $primary-color;
+			font-size: clamp(0vw, 3rem, 8vw);
+			font-weight: 900;
+			margin-bottom: 2rem;
+			text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
+		}
+
+		.form-container {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			width: 100%;
+			max-width: 1200px;
+			margin: 0 auto;
+			.character-form {
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				width: 40%;
+				gap: 1rem;
+
+				img {
+					width: 100%;
+					max-width: 300px;
+					border-radius: 12px;
+				}
+
+				.character-form-buttons {
+					display: flex;
+					width: 100%;
+					gap: 1rem;
+					button {
+						border-radius: 12px;
+						padding: 1rem;
+						font-size: 1.2rem;
+						font-weight: 900;
+						background-color: $secondary-color;
+						color: white;
+						border: none;
+						cursor: pointer;
+						width: 100%;
+						&:hover {
+							background-color: darken($secondary-color, 10%);
+						}
+					}
+				}
+			}
+
+			.room-form {
+				display: flex;
+				flex-direction: column;
+				width: 40%;
+				gap: 1rem;
+
+				input {
+					border-radius: 12px;
+					width: 100%;
+					height: 3.2rem;
+					font-size: clamp(0rem, 1.1rem, 80%);
+					font-weight: 600;
+					background-color: $secondary-color;
+					text-align: center;
+					color: white;
+					border: none;
+				}
+
+				button {
+					margin-top: 2rem;
+					border-radius: 12px;
+					padding: 1rem 1.5rem;
+					font-size: 1.2rem;
+					font-weight: 600;
+					background-color: $secondary-color;
+					color: white;
+					border: none;
+					cursor: pointer;
+					text-transform: uppercase;
+
+					&:hover {
+						background-color: darken($secondary-color, 10%);
+					}
+				}
+			}
+		}
 	}
-
-    .login-container {
-        padding: 3rem;
-
-	    h1 {
-	    	color: $primary-color;
-	    	font-size: 4rem;
-	    	font-weight: 900;
-	    	margin-bottom: 2rem;
-	    	text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
-	    }
-
-        .form-container {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            width: 100%;
-            max-width: 1200px;
-            margin: 0 auto;
-        }
-
-        .character-form {
-            justify-content: center;
-            align-items: center;
-            margin-bottom: 2rem;
-            width: 40%;
-
-            img {
-                width: 100%;
-                max-width: 300px;
-                border-radius: 12px;
-                box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
-            }
-
-            .character-form-buttons {
-                button {
-                    border-radius: 12px;
-                    padding: 1rem;
-                    font-size: 1.2rem;
-                    font-weight: 600;
-                    background-color: $secondary-color;
-                    color: white;
-                    border: none;
-                    cursor: pointer;
-                    width: 49%;
-                    
-                    &:hover {
-                        background-color: darken($secondary-color, 10%);
-                    }
-                }
-            }
-        }
-
-	    .room-form {
-	    	display: flex;
-	    	flex-direction: column;
-            margin-left: auto; 
-            margin-right: 0;
-            width: 50%;
-            
-	    	gap: 1rem;
-
-	    	input {
-	    		border-radius: 12px;
-	    		padding: 1rem 2rem;
-	    		font-size: 1.1rem;
-	    		font-weight: 600;
-	    		background-color: $secondary-color;
-	    		text-align: center;
-	    		color: white;
-	    		border: none;
-	    	}
-
-	    	button {
-	    		margin-top: 2rem;
-	    		border-radius: 12px;
-	    		padding: 1rem 1.5rem;
-	    		font-size: 1.2rem;
-	    		font-weight: 600;
-	    		background-color: $secondary-color;
-	    		color: white;
-	    		border: none;
-	    		cursor: pointer;
-	    		text-transform: uppercase;
-
-	    		&:hover {
-	    			background-color: darken($secondary-color, 10%);
-	    		}
-	    	}
-        }
-    }
 </style>
