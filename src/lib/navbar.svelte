aaf57a9e9fe02092aadc6882d0dd64707f2caa42
endercass <46408156+Endercass@users.noreply.github.com>
2023-07-10 03:33:15 -0400

    fix css rendering on chromium

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div class="navbar">
-	<div class="logo" on:click={() => goto('/', { replaceState: false })}>
+	<a class="logo" href="/">
 		<img src="obg-logo-placeholder.png" alt="Logo" />
-	</div>
+	</a>
 
 	<div class="buttons">
 		<button on:click={() => goto('/login', { replaceState: false })}>Login</button>
@@ -23,35 +23,37 @@
 		justify-content: space-between;
 		background-color: $primary-color;
 		height: 6vh;
+		min-height: 2rem;
 		width: 100%;
 		color: white;
 		font-size: 24px;
 
 		.logo {
-			font-weight: bold;
 			cursor: pointer;
+			height: 100%;
 
 			img {
-				height: 8vh;
+				height: 100%;
 				width: auto;
 			}
 		}
 
 		.buttons {
 			display: flex;
-			height: 100%;
+			flex-direction: row;
 			align-items: center;
 			gap: 0.5rem;
-			margin: 0 0.5rem;
+			height: 100%;
+			margin: 0.5rem;
 			button {
 				height: calc(100% - 1rem);
 				background-color: $secondary-color;
-				padding: 0.5rem 1.5rem;
+				padding: 0 1.5rem;
 				color: white;
 				border: none;
 				border-radius: 4px;
 				cursor: pointer;
-				font-size: 1.2rem;
+				font-size: 1.1rem;
 
 				&:hover {
 					background-color: darken($secondary-color, 10%);
