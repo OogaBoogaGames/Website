bab0662e7c54b706eee2c1013de6aacf865997fb
endercass <46408156+Endercass@users.noreply.github.com>
2023-10-16 21:26:55 -0400

    Migrate navbar to tailwind and add icon

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -2,63 +2,13 @@
 	import { goto } from '$app/navigation';
 </script>
 
-<div class="navbar">
-	<a class="logo" href="/">
-		<img src="/obg-logo-placeholder.webp" alt="Logo" />
+<div class="navbar flex items-center justify-between bg-[#9bad8c] h-[6vh] min-h-[2rem] w-full text-stone-100 text-2xl">
+	<a class="logo cursor-pointer h-full" href="/">
+		<img class="h-full w-auto" src="/favicon.webp" alt="Logo" />
 	</a>
 
-	<div class="buttons">
-		<button on:click={() => goto('/login', { replaceState: false })}>Login</button>
-		<button on:click={() => goto('/signup', { replaceState: false })}>Signup</button>
+	<div class="buttons flex flex-row items-center gap-2 h-full m-2">
+		<button class="h-[calc(100%_-_1rem)] bg-[#6d7a62] p-[0_1.5rem] text-stone-100 rounded cursor-pointer text-[1.1rem] hover:bg-[#626e58]" on:click={() => goto('/login', { replaceState: false })}>Login</button>
+		<button class="h-[calc(100%_-_1rem)] bg-[#6d7a62] p-[0_1.5rem] text-stone-100 rounded cursor-pointer text-[1.1rem] hover:bg-[#626e58]" on:click={() => goto('/signup', { replaceState: false })}>Signup</button>
 	</div>
 </div>
-
-<style lang="scss">
-	$primary-color: #9bad8c;
-	$secondary-color: #6d7a62;
-
-	.navbar {
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		background-color: $primary-color;
-		height: 6vh;
-		min-height: 2rem;
-		width: 100%;
-		color: white;
-		font-size: 24px;
-
-		.logo {
-			cursor: pointer;
-			height: 100%;
-
-			img {
-				height: 100%;
-				width: auto;
-			}
-		}
-
-		.buttons {
-			display: flex;
-			flex-direction: row;
-			align-items: center;
-			gap: 0.5rem;
-			height: 100%;
-			margin: 0.5rem;
-			button {
-				height: calc(100% - 1rem);
-				background-color: $secondary-color;
-				padding: 0 1.5rem;
-				color: white;
-				border: none;
-				border-radius: 4px;
-				cursor: pointer;
-				font-size: 1.1rem;
-
-				&:hover {
-					background-color: darken($secondary-color, 10%);
-				}
-			}
-		}
-	}
-</style>
